94b0d804d15856f97afd6bab00371f0abacb414d
Victor Oliveira <victor.gso001@gmail.com>
2025-10-25 21:30:45 -0300

    Created main routes

--- src/main.py
+++ src/main.py
@@ -1,35 +1,122 @@
-from fastapi import FastAPI
-from database import InMemoryDatabase
-
+"""
+    API's main module
+"""
+import pickle
+from typing import Optional
 import uvicorn
+import numpy as np
+from fastapi import FastAPI, File, HTTPException
+from sklearn.linear_model import LinearRegression
+from src.models import Inputs
+# from src.database import InMemoryDatabase
 
 
 app = FastAPI()
+MODEL = Optional[LinearRegression]
 
 
-@app.get("/health", status_code=200, tags=["health"], summary="Health check")
-async def health():
+@app.get(
+    "/health",
+    status_code=200,
+    tags=["healthcheck"],
+    summary="Health check"
+)
+def healthcheck():
+    """
+        API healthcheck
+    """
     return {"status": "ok"}
 
-@app.post("/user/", tags=["example"], summary="Insert user")
-async def insert(data: dict):
-    db = InMemoryDatabase()
-    users = db.get_collection('users')
-    users.insert_one(data)
-    return {"status": "ok"}
 
-@app.get("/user/{name}", status_code=200, tags=["example"], summary="Get user by name")
-async def get(name: str):
-    db = InMemoryDatabase()
-    users = db.get_collection('users')
-    user = users.find_one({"name": name})
-    return {"status": "ok", "user": user}
+@app.post(
+    "/model/load",
+    status_code=200,
+    tags=["model_load"],
+    summary="Loads a model into API for prediction"
+)
+def model_load(data: bytes = File(...)):
+    """
+        Route for model loading as pickle file
+    """
+    global MODEL
+    try:
+        MODEL = pickle.loads(data)
+    except Exception as e:
+        raise HTTPException(
+            status_code=400,
+            detail="Cannot load model"
+        ) from e
 
-@app.get("/user/", tags=["example"], summary="List all users")
-async def list():
-    db = InMemoryDatabase()
-    users = db.get_collection('users')
-    return {"status": "ok", "users": [x for x in users.find({},{"_id": 0})]}
+    print(MODEL, flush=True)
+
+    if not isinstance(MODEL, LinearRegression):
+        raise HTTPException(
+            status_code=403,
+            detail="Cannot load model."
+        )
+    return {"message": "Model loaded successfully"}
+
+
+@app.post(
+    "/model/predict",
+    tags=["Model Prediction"],
+    summary="Predict arrival delay based on input variables"
+)
+def predict(inputs: Inputs):
+    """
+        Model prediction route
+    """
+    global MODEL
+    values = [
+        inputs.dep_time,
+        inputs.sched_dep_time,
+        inputs.dep_delay,
+        inputs.sched_arr_time,
+        inputs.distance,
+        inputs.wind_speed_origin,
+        inputs.wind_speed_dest
+    ]
+
+    predict = MODEL.predict(np.array(values).reshape(1, -1))
+    print(predict)
+    return {"message": {
+        "arr_delay": predict[0]
+    }}
+# @app.post("/user/", tags=["example"], summary="Insert user")
+# async def insert(data: dict):
+#     """
+#         Insert user to database
+#     """
+#     db = InMemoryDatabase()
+#     users = db.get_collection('users')
+#     users.insert_one(data)
+#     return {"status": "ok"}
+
+
+# @app.get(
+#   "/user/{name}",
+#   status_code=200,
+#   tags=["example"],
+#   summary="Get user by name"
+# )
+# async def get(name: str):
+#     """
+#         Get user from database
+#     """
+#     db = InMemoryDatabase()
+#     users = db.get_collection('users')
+#     user = users.find_one({"name": name})
+#     return {"status": "ok", "user": user}
+
+
+# @app.get("/user/", tags=["example"], summary="List all users")
+# async def user_list():
+#     """
+#         Get all users from database
+#     """
+#     db = InMemoryDatabase()
+#     users = db.get_collection('users')
+#     return {"status": "ok", "users": list(users.find({}, {"_id": 0}))}
 
 
 if __name__ == "__main__":
